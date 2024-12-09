934b752fc827b6ff0dc4b0cbb931494292468f71
Johan <johanotr@gmail.com>
2024-12-09 14:40:51 -0500

    chore: sync repo with remote

--- src/components/store/Shop.jsx
+++ src/components/store/Shop.jsx
@@ -16,7 +16,8 @@ export const Shop = () => {
     <>
     <Header />
     
-    
+    <p style={{padding: "10px"}}>shop&gt;Category&gt;Eletronic</p>
+
     <div className={styles["product-container"]}>
       {products.map(product => <Product key={product.id} {...product} /> )}
     </div>
