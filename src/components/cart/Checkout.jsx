ad2ec732858c273a38cb35d1ed20b705260400d9
Johan <johanotr@gmail.com>
2024-12-16 11:38:02 -0500

    feat: add desktop styling to checkout

--- src/components/cart/Checkout.jsx
+++ src/components/cart/Checkout.jsx
@@ -13,26 +13,26 @@ export const Checkout = () => {
     <>
       <div className={styles.header}>
         <ChevronLeft className={styles["back-arrow"]} size={34} onClick={() => navigate(-1)}/>
-        <h1>Cart</h1>
+        <h1 className={styles["checkout-title"]}>Cart</h1>
       </div>
 
       <div className={styles.main}>
         {Object.keys(cartProducts).length === 0
-          ?   <>
+          ?   <div>
                 <p><i>Your cart is empty</i></p>
                 <Link to={"/shop"} ><button className={styles["shop-btn"]}>Shop Now</button></Link>
-              </>
+              </div>
           :   <>
                 <div className={styles.products}>
                   {Object.values(cartProducts).map(product => {
                    return <div key={product.id} className={styles.product}>
                             <div className={styles["image-wrapper"]}><img src={product.image} alt="" /></div>
-                            <p>{product.title}</p>
+                            <div className={styles.title}><p>{product.title}</p></div>
                             <div className={styles.price}>
                               <p>${product.price}</p>
                             </div>
                             <div className={styles["quantity-bar"]}>
-                              <button onClick={() => removeCartProducts(product.title)}>{product.quantity < 2 ? <Trash color="#aa2016" /> :<MinusCircleIcon />}</button>
+                              {product.quantity < 2 ? <Trash color="#aa2016" onClick={() => removeCartProducts(product.title)}/> :<MinusCircleIcon onClick={() => removeCartProducts(product.title)} />}
                               <p>{product.quantity}</p>
                               <PlusCircleIcon onClick={() => updateCartProducts(product.title, product.quantity, product.image, product.price, product.id)} />
                             </div>
@@ -41,11 +41,20 @@ export const Checkout = () => {
                 </div>
 
                 <div className={styles["checkout-wrapper"]}>
-
-                  <button className={styles["checkout-btn"]}>Checkout</button>
-                  <div className={styles["final-price"]}>
-                    Total <span className={styles["final-price-num"]}>${finalPrice.toFixed(2)}</span>
+                  <div className={styles.subtotal}>
+                    <p>subtotal</p>
+                    <p>${finalPrice.toFixed(2)}</p>
                   </div>
+                  <div className={styles.tax}>
+                    <p>estimated tax</p>
+                    <p>${(finalPrice * .0625).toFixed(2)}</p>
+                  </div>
+                  <div className={styles["final-price"]}>
+                      <p>Total</p>
+                      <p>${(finalPrice + (finalPrice * .0625)).toFixed(2)}</p>
+                  </div>
+                  <button className={styles["checkout-btn"]}>Checkout</button>
+                  
                   
                 </div>
 
